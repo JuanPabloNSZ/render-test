4a46f103437bbbe939a5ad84198adae947058e13
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-02-09 21:49:43 -0300

    add express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
 let notes = [
   {
@@ -18,12 +19,15 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  // JSON.stringify() convierte las notas en formato JSON
-  response.end(JSON.stringify(notes));
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on Port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
