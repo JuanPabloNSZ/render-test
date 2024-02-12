bd9b2fd152269c206bbd91d51b4459eecfc538f0
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2024-02-12 19:58:45 -0300

    add functionality for post notes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let notes = [
   {
@@ -43,6 +44,34 @@ app.get("/api/notes/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+const generateId = () => {
+  // map itera a través de cada elemento de notes y almacena las propiedades id en un nuevo array
+  // se usa spread syntax (...) para desempaquetar los elementos de ese array
+  // para que puedan ser usados como argumentos en Math.max()
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
 });
 
 app.delete("/api/notes/:id", (request, response) => {
